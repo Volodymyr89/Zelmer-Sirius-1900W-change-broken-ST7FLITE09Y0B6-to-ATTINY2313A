28f40245c9ca0a9c132b2fc0ebbe126dd29147d5
Volodymyr <volodymyrb89@gmail.com>
2023-06-15 15:54:43 +0300

    Update main.c, add comments

--- Zelmer/Zelmer/main.c
+++ Zelmer/Zelmer/main.c
@@ -13,58 +13,59 @@
 #include "periphery_init.h"
 #include "motor_and_LED_control.h"
 
-bool button2_latch=false, button3_latch=false;
+bool button2_latch=false, button3_latch=false;// latch for buttons
 
 int main(void)
 {	
-	Output_Pins_Init();
+	Output_Pins_Init(); // init output PINs
+	//set filter LEDs for operation check 
 	SET_LED5;// set LED
 	SET_LED6;// set LED
-	Input_Pins_Init();
-	Timer0_Init();
-	Timer1_Init();
+	//set filter LEDs for operation check end
+	Input_Pins_Init(); // init input pins
+	Timer0_Init(); // init Timer0
+	Timer1_Init(); // init Timer1
 	sei();//enable global interrupts
-	Soft_Start_and_Run_to_Max();
+	Soft_Start_and_Run_to_Max(); // start motor and increase power up to max
 	RESET_LED5;// reset LED
 	RESET_LED6;// reset LED
 	
-
     while (1) 
-    {
-
+    {		//check if increment button is pressed
 		if((!(PIND&(1<<PIND2))) && (!button2_latch)){
 			button2_latch=true;
-			while((uint8_t)1==Delay_ms(200,(PIND&(1<<PIND2)))){}
-				Increment_Power_and_LEDs();
+			while((uint8_t)1==Delay_ms(200,(PIND&(1<<PIND2)))){} // deglitch button
+				Increment_Power_and_LEDs(); increment LED status bar and power to motor
 			}
+	       //check if decrement button is pressed
 		else if((!(PIND&(1<<PIND3))) && (!button3_latch)){
 				button3_latch=true;
-				while((uint8_t)1==Delay_ms(200,(PIND&(1<<PIND3)))){}
-				Decrement_Power_and_LEDs();
+				while((uint8_t)1==Delay_ms(200,(PIND&(1<<PIND3)))){} // deglitch button
+				Decrement_Power_and_LEDs(); decrement LED status bar and power to motor
 		}
 		if(PIND&(1<<PIND2)){
-			button2_latch=false;
+			button2_latch=false; // release latch
 		}
 		if(PIND&(1<<PIND3)){
-			button3_latch=false;
+			button3_latch=false; // release latch
 		}
 		if(!(PINA&(1<<PINA1))){
-				SET_LED5;// set LED
+			SET_LED5;   // set LED set led if filter is dirty
 		}
 		else{
 			RESET_LED5;// reset LED
 		}
 		if(!(PIND&(1<<PIND1))){
-			SET_LED6;// set LED
+			SET_LED6;  // set LED set led if filter is dirty
 		}
 		else{
 			RESET_LED6;// reset LED
 		}
 		if(!(PINA&(1<<PINA0))){
-			SET_SOCKET;// reset socket
+			SET_SOCKET;   // turn ON socket for brush
 		}
 		else{
-			 RESET_SOCKET;// reset socket
+			RESET_SOCKET; // turn OFF socket for brush
 		}	
 	}
 		
